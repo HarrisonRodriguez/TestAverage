80f7694d4dc7a070846f7a875f0b6abe98fe7950
Harrison Rodriguez <harrisonmrod@gmail.com>
2019-09-06 13:13:00 -0500

    Test Average Program Complete

--- TestAverage/TestAverage/Program.cs
+++ TestAverage/TestAverage/Program.cs
@@ -6,7 +6,25 @@ namespace TestAverage
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //Test Average App
+
+            Console.WriteLine("What is your first test score?");
+            //Input from Console is of type string. So we must convert it to an int
+            var responseFromUser = int.Parse(Console.ReadLine());
+            Console.WriteLine("What is your second test score?");
+            var responseFromUser2 = int.Parse(Console.ReadLine());
+            Console.WriteLine("What is your third and final test score?");
+            var responseFromUser3 = int.Parse(Console.ReadLine());
+
+            var responsesAdded = (responseFromUser + responseFromUser2 + responseFromUser3);
+            var testAverage = (responsesAdded / 3);
+
+            Console.WriteLine("The average of your three test scores is..." + testAverage);
+         
+
+
+            Console.ReadLine();
+
         }
     }
 }
